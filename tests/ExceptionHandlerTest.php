856f889b770c86ba1ffe25f944fc35fd2101d238
Julien <julien@themosis.com>
2024-11-12 13:24:00 +0100

    Cover exception handler code.

--- tests/ExceptionHandlerTest.php
+++ tests/ExceptionHandlerTest.php
@@ -10,11 +10,7 @@ namespace Themosis\Components\Error\Tests;
 
 use Exception;
 use PHPUnit\Framework\Attributes\Test;
-use Themosis\Components\Error\Backtrace\Backtrace;
-use Themosis\Components\Error\Backtrace\InMemoryFrameIdentifiers;
-use Themosis\Components\Error\ExceptionalIssue;
-use Themosis\Components\Error\ExceptionHandlerHttpResponse;
-use Themosis\Components\Error\InMemoryInformation;
+use Themosis\Components\Error\ExceptionHandler;
 use Themosis\Components\Error\InMemoryIssues;
 use Themosis\Components\Error\InMemoryReporters;
 use Themosis\Components\Error\Issue;
@@ -27,18 +23,14 @@ final class ExceptionHandlerTest extends TestCase
     #[Test]
     public function it_can_report_thrown_exception(): void
     {
+        $stdout = '';
+
         $reporters = new InMemoryReporters();
         $reporters->add(
             condition: new AlwaysReport(),
             reporter: new CallbackReporter(
-                static function (Issue $issue) {
-                    $backtrace = new Backtrace(new InMemoryFrameIdentifiers());
-                    $backtrace->captureException($issue->exception());
-
-                    ( new ExceptionHandlerHttpResponse(
-                        backtrace: $backtrace,
-                        information: new InMemoryInformation(),
-                    ) )->render($issue);
+                static function (Issue $issue) use (&$stdout) {
+                    $stdout = $issue->message();
                 }
             ),
         );
@@ -48,15 +40,9 @@ final class ExceptionHandlerTest extends TestCase
             issues: new InMemoryIssues(),
         );
 
-        $reportHandler->capture(
-            ExceptionalIssue::create(new Exception('Oops'))
-        );
+        $exceptionHandler = new ExceptionHandler($reportHandler);
+        $exceptionHandler(new Exception('Oops'));
 
-        ob_start();
-            $reportHandler->publish();
-        $stdout = ob_get_clean();
-
-        $this->assertTrue(str_contains($stdout, 'Oops'));
-        $this->assertTrue(str_contains($stdout, 'Issue'));
+        $this->assertSame('Oops', $stdout);
     }
 }
